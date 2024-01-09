409a5f2b7c7d2ac5cdb34dde73fc8c84390c36fa
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-09 19:45:50 -0300

    se agregan datos del empleado y medidas responsive para imagen del avatar

--- components/FilaEmpleado.jsx
+++ components/FilaEmpleado.jsx
@@ -6,7 +6,13 @@ const FilaEmpleado = () => {
   return (
     <ListGroupItem className='d-flex justify-content-start'>
       <AvatarEmpleado />
-      
+      <div className="d-flex flex-column ms-2 mt-3">
+        <h2>Laya Dueñas</h2>
+        <div className='d-flex'>
+          <p className='m-0 me-2'>CEO</p>
+          <p className='m-0 px-1 area'>Business</p>
+        </div>
+      </div>
     </ListGroupItem>
   )
 }
