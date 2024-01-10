865a6bf8e0bd5eed5f57414b0080aa8aeea27f33
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-10 15:49:54 -0300

    se corrige maquetado

--- components/FilaEmpleado.jsx
+++ components/FilaEmpleado.jsx
@@ -6,10 +6,10 @@ const FilaEmpleado = ({empleado}) => {
   return (
     <ListGroupItem className='d-flex justify-content-start'>
       <AvatarEmpleado empleado={empleado} />
-      <div className="d-flex flex-column ms-2 mt-3">
+      <div className="d-flex flex-column ms-1 mt-3 w-100">
         <h2>{empleado.fullName}</h2>
-        <div className='d-flex'>
-          <p className='m-0 me-2'>{empleado.title}</p>
+        <div className='d-flex justify-content-between'>
+          <p className='m-0 me-1'>{empleado.title}</p>
           <p className='m-0 px-1 area'>{empleado.department}</p>
         </div>
       </div>
